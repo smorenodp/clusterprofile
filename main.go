d233bbc7818007a20e2d9393fc0ebd1013ad1d97
Sergio Moreno <sergio.moreno.pradas.tech@bbva.com>
2024-04-17 12:35:59 +0200

    fix: check if profile exists

--- main.go
+++ main.go
@@ -45,56 +45,59 @@ func main() {
 	profiles, err := config.ReadConfig(configFolder)
 
 	if err != nil {
-		log.Fatalf("Error parsing config file from folder %s - %s\n", configFolder, err)
+		errorLog.Fatalf("Error parsing config file from folder %s - %s\n", configFolder, err)
 	}
 
 	creds, err := config.LoadCreds(credsFile)
 	if err != nil {
-		log.Fatalf("Error parsing creds file from %s\n", credsFile)
+		errorLog.Fatalf("Error parsing creds file from %s\n", credsFile)
 	}
 
-	c := profiles[profileName]
-	client, err = providers.NewVaultClient(c.Vault)
-	client.LoadProfileCreds(creds[profileName])
-	if c.Vault.PivotProfile != "" {
-		if clusterConfig, ok := profiles[c.Vault.PivotProfile]; !ok {
-			log.Fatalf("Error loading profile pivoting %s in profile %s configuration", c.Vault.PivotProfile, c.Name)
+	if c, ok := profiles[profileName]; !ok {
+		errorLog.Fatalf("Error loading profile %s, not found in configuration folder %s\n", profileName, configFolder)
+	} else {
+		client, err = providers.NewVaultClient(c.Vault)
+		client.LoadProfileCreds(creds[profileName])
+		if c.Vault.PivotProfile != "" {
+			if clusterConfig, ok := profiles[c.Vault.PivotProfile]; !ok {
+				log.Fatalf("Error loading profile pivoting %s in profile %s configuration", c.Vault.PivotProfile, c.Name)
+			} else {
+				client = client.WithPivotRole(clusterConfig.Vault, creds[c.Vault.PivotProfile])
+				creds[c.Vault.PivotProfile] = client.ProfileCreds()
+				client.GenerateCreds()
+			}
 		} else {
-			client = client.WithPivotRole(clusterConfig.Vault, creds[c.Vault.PivotProfile])
-			creds[c.Vault.PivotProfile] = client.ProfileCreds()
 			client.GenerateCreds()
 		}
-	} else {
-		client.GenerateCreds()
-	}
-	if err != nil {
-		log.Fatal("Vault creation failed")
-	}
-	exportCreds := []string{fmt.Sprintf("export %s=%s", clusterProfileEnv, profileName)}
-	profileCreds = client.ProfileCreds()
-	exportCreds = client.ExportCreds()
-	for _, p := range c.Providers {
-		provider := providers.NewProvider(client, p)
-		if provider != nil {
-			provider.LoadProfileCreds(creds[profileName])
-			if !provider.CredsLoaded() {
-				provider.GenerateCreds()
-			}
-			exportCreds = append(exportCreds, provider.ExportCreds()...)
-			profileCreds = append(profileCreds, provider.ProfileCreds()...)
-		} else {
-			errorLog.Println("Provider of type %s not implemented\n", p.Type)
+		if err != nil {
+			log.Fatal("Vault creation failed")
 		}
-	}
-	creds[profileName] = profileCreds
+		exportCreds := []string{fmt.Sprintf("export %s=%s", clusterProfileEnv, profileName)}
+		profileCreds = client.ProfileCreds()
+		exportCreds = client.ExportCreds()
+		for _, p := range c.Providers {
+			provider := providers.NewProvider(client, p)
+			if provider != nil {
+				provider.LoadProfileCreds(creds[profileName])
+				if !provider.CredsLoaded() {
+					provider.GenerateCreds()
+				}
+				exportCreds = append(exportCreds, provider.ExportCreds()...)
+				profileCreds = append(profileCreds, provider.ProfileCreds()...)
+			} else {
+				errorLog.Println("Provider of type %s not implemented\n", p.Type)
+			}
+		}
+		creds[profileName] = profileCreds
 
-	if err = config.SaveCreds(credsFile, creds); err != nil {
-		log.Fatalf("Error saving credentials in %s - %s", credsFile, err)
-	}
+		if err = config.SaveCreds(credsFile, creds); err != nil {
+			log.Fatalf("Error saving credentials in %s - %s", credsFile, err)
+		}
 
-	if echo {
-		config.GenerateExportContent(profileName, exportCreds, banner)
-	} else {
-		config.CreateExecFile(execFile, profileName, exportCreds, banner)
+		if echo {
+			config.GenerateExportContent(profileName, exportCreds, banner)
+		} else {
+			config.CreateExecFile(execFile, profileName, exportCreds, banner)
+		}
 	}
 }
